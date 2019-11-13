e6ba8b979cdc831431195166e093ab6755945dce
jingyaopu <jingyaopu@gmail.com>
2019-11-13 03:23:33 -0800

    Updates

---
+++ precache-manifest.882948f62f30be74ccababecdb49cd53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0810557877cf90f6a15f785d12b5deb",
+    "url": "/Char-Converter/index.html"
+  },
+  {
+    "revision": "39b5994f0aafa6e4c89a",
+    "url": "/Char-Converter/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4f655535e308fadee5d3",
+    "url": "/Char-Converter/static/js/2.4bae3f60.chunk.js"
+  },
+  {
+    "revision": "39b5994f0aafa6e4c89a",
+    "url": "/Char-Converter/static/js/main.0ff22b54.chunk.js"
+  },
+  {
+    "revision": "393d3625a5694d33cd67",
+    "url": "/Char-Converter/static/js/runtime-main.dce976e9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Char-Converter/static/media/logo.25bf045c.svg"
+  }
+]);
